e37a84caf4e9213e3ef58b8146f9c9febf0f8e09
Izbenko <izbenko1993@bk.ru>
2022-07-26 02:43:08 +0300

    update

--- bjs/10_function_object/init.js
+++ bjs/10_function_object/init.js
@@ -1,6 +1,5 @@
 
-window.onload = function()
-{
+document.getElementById('generator').addEventListener('click', function () {
     const initPerson = personGenerator.getPerson();
     document.getElementById('firstNameOutput').innerText = initPerson.firstName;
     document.getElementById('surnameOutput').innerText = initPerson.surname;
@@ -8,5 +7,14 @@ window.onload = function()
     document.getElementById('bornOutput').innerText = initPerson.born;
     document.getElementById('professionOutput').innerText = initPerson.profession;
     document.getElementById('patronymicOutput').innerText = initPerson.patronymic;
-};
+})
+
+document.getElementById('reset').addEventListener('click', function () {
+    document.getElementById('firstNameOutput').innerText = '-';
+    document.getElementById('surnameOutput').innerText = '-';
+    document.getElementById('genderOutput').innerText = '-';
+    document.getElementById('bornOutput').innerText = '-';
+    document.getElementById('professionOutput').innerText = '-';
+    document.getElementById('patronymicOutput').innerText = '-';
+})
 
