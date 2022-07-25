672e2dcca57b6fe7232971d2aba821a7936e0f25
Izbenko <izbenko1993@bk.ru>
2022-07-26 00:40:25 +0300

    update

--- bjs/10_function_object/personGenerator.js
+++ bjs/10_function_object/personGenerator.js
@@ -36,6 +36,22 @@ const personGenerator = {
         }
     }`,
 
+    firstNameFamaleJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Александра",
+            "id_2": "Мария",
+            "id_3": "Ирина",
+            "id_4": "Алена",
+            "id_5": "Дарья",
+            "id_6": "Наталья",
+            "id_7": "Ксения",
+            "id_8": "Оксана",
+            "id_9": "Елена",
+            "id_10": "Анастасия"
+        }
+    }`,
+
     GENDER_MALE: 'Мужчина',
     GENDER_FEMALE: 'Женщина',
 
@@ -47,24 +63,44 @@ const personGenerator = {
         return obj.list[prop];
     },
 
-    randomFirstName: function() {
+    randomFirstName: function(gender) {
 
-        return this.randomValue(this.firstNameMaleJson);
+        if (gender == this.GENDER_MALE) {
+            return this.randomValue(this.firstNameMaleJson);
+        } else {
+            return this.randomValue(this.firstNameFamaleJson);
+        }
 
     },
 
 
-     randomSurname: function() {
+     randomSurname: function(gender) {
 
-        return this.randomValue(this.surnameJson);
+        if (gender == this.GENDER_FEMALE) {
+            return this.randomValue(this.surnameJson) + 'а';
+        } else {
+            return this.randomValue(this.surnameJson);
+        }
+
+    },
+
+    randomGender: function() {
+
+        let g = this.randomIntNumber();
+        if (g == 1) {
+            return this.GENDER_MALE;
+        } else {
+            return this.GENDER_FEMALE;
+        }
 
     },
 
 
     getPerson: function () {
         this.person = {};
-        // this.person.gender = this.randomGender();
-        this.person.firstName = this.randomFirstName();
+        this.person.gender = this.randomGender();
+        this.person.firstName = this.randomFirstName(this.person.gender);
+        this.person.surname = this.randomSurname(this.person.gender);
         return this.person;
     }
 };
